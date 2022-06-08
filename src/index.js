ad4d8afcd026f1bd0c79340cb641e62d5d9733d3
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2022-06-08 17:53:19 +0200

    UPDATE: moved from previous location and added new imports

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/* STYLED COMPONETS */
+import Button from './components/buttons/Button';
+
+/* STYLE */
+import { fSize } from './style/font/fSize';
+import { m } from './style/alignment/margin/margin';
+import { p } from './style/alignment/padding/padding';
+import fColor from './style/font/fColor';
+
+
+export {
+  // * STYLED COMPONENTS
+  Button,
+
+  // * STYLE
+  // font
+  fSize,
+  fColor,
+
+  // alignment
+  m,
+  p,
+}
