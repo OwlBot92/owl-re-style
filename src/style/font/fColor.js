9deca77c1794addf3ede574718b8a4580c2b9d01
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2022-06-08 17:54:37 +0200

    NEW: font colors

---
+++ src/style/font/fColor.js
@@ -0,0 +1,195 @@
+import { StyleSheet } from 'react-native';
+import blue from '../color/palette/blue';
+import red from '../color/palette/red';
+import green from '../color/palette/green';
+import yellow from '../color/palette/yellow';
+import { grey, white, black } from '../color/palette/grey';
+
+const fColor = StyleSheet.create({
+  /* RED */
+  red50: {
+    color: red._50,
+  },
+  red100: {
+    color: red._100,
+  },
+  red200: {
+    color: red._200,
+  },
+  red300: {
+    color: red._300,
+  },
+  red400: {
+    color: red._400,
+  },
+  red500: {
+    color: red._500,
+  },
+  red600: {
+    color: red._600,
+  },
+  red700: {
+    color: red._700,
+  },
+  red800: {
+    color: red._800,
+  },
+  red900: {
+    color: red._900,
+  },
+  red1000: {
+    color: red._1000,
+  },
+
+  /* BLUE */
+  blue50: {
+    color: blue._50,
+  },
+  blue100: {
+    color: blue._100,
+  },
+  blue200: {
+    color: blue._200,
+  },
+  blue300: {
+    color: blue._300,
+  },
+  blue400: {
+    color: blue._400,
+  },
+  blue500: {
+    color: blue._500,
+  },
+  blue600: {
+    color: blue._600,
+  },
+  blue700: {
+    color: blue._700,
+  },
+  blue800: {
+    color: blue._800,
+  },
+  blue900: {
+    color: blue._900,
+  },
+  blue1000: {
+    color: blue._1000,
+  },
+
+  /* GREEN */
+  green50: {
+    color: green._50,
+  },
+  green100: {
+    color: green._100,
+  },
+  green200: {
+    color: green._200,
+  },
+  green300: {
+    color: green._300,
+  },
+  green400: {
+    color: green._400,
+  },
+  green500: {
+    color: green._500,
+  },
+  green600: {
+    color: green._600,
+  },
+  green700: {
+    color: green._700,
+  },
+  green800: {
+    color: green._800,
+  },
+  green900: {
+    color: green._900,
+  },
+  green1000: {
+    color: green._1000,
+  },
+
+  /* YELLOW */
+  yellow50: {
+    color: yellow._50,
+  },
+  yellow100: {
+    color: yellow._100,
+  },
+  yellow200: {
+    color: yellow._200,
+  },
+  yellow300: {
+    color: yellow._300,
+  },
+  yellow400: {
+    color: yellow._400,
+  },
+  yellow500: {
+    color: yellow._500,
+  },
+  yellow600: {
+    color: yellow._600,
+  },
+  yellow700: {
+    color: yellow._700,
+  },
+  yellow800: {
+    color: yellow._800,
+  },
+  yellow900: {
+    color: yellow._900,
+  },
+  yellow1000: {
+    color: yellow._1000,
+  },
+
+  /* GREY */
+  grey50: {
+    color: grey._50,
+  },
+  grey100: {
+    color: grey._100,
+  },
+  grey200: {
+    color: grey._200,
+  },
+  grey300: {
+    color: grey._300,
+  },
+  grey400: {
+    color: grey._400,
+  },
+  grey500: {
+    color: grey._500,
+  },
+  grey600: {
+    color: grey._600,
+  },
+  grey700: {
+    color: grey._700,
+  },
+  grey800: {
+    color: grey._800,
+  },
+  grey900: {
+    color: grey._900,
+  },
+  grey1000: {
+    color: grey._1000,
+  },
+
+  /* WHITE */
+  white: {
+    color: white._50,
+  },
+
+  /* BLACK */
+  black: {
+    color: black._50,
+  },
+});
+
+export default fColor;
