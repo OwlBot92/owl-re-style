a85a3da0a8d097c5a012b2613d4fb297c1d886b7
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2022-06-17 02:56:31 +0200

    NEW: font weight options

---
+++ src/style/font/fWeight.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+export const fWeight = StyleSheet.create({
+  /* FONT WEIGHT */
+  normal: {
+    fontWeight: 'normal',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  _100: {
+    fontWeight: 100,
+  },
+  _200: {
+    fontWeight: 200,
+  },
+  _300: {
+    fontWeight: 300,
+  },
+  _400: {
+    fontWeight: 400,
+  },
+  _500: {
+    fontWeight: 500,
+  },
+  _600: {
+    fontWeight: 600,
+  },
+  _700: {
+    fontWeight: 700,
+  },
+  _800: {
+    fontWeight: 800,
+  },
+  _900: {
+    fontWeight: 900,
+  },
+});
+
