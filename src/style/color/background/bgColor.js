c5f6e279519f6ef8a5409f71b0b2bf7f6a554a28
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2022-06-17 02:55:28 +0200

    NEW: background colors

---
+++ src/style/color/background/bgColor.js
@@ -0,0 +1,196 @@
+import { StyleSheet } from "react-native";
+
+import blue from "../palette/blue";
+import red from "../palette/red";
+import green from "../palette/green";
+import yellow from "../palette/yellow";
+import { grey, white, black } from "../palette/grey";
+
+export const bgColor = StyleSheet.create({
+  /* BACKGROUND COLOR */
+
+  /* BLUE */
+  blue50: {
+    backgroundColor: blue._50,
+  },
+  blue100: {
+    backgroundColor: blue._100,
+  },
+  blue200: {
+    backgroundColor: blue._200,
+  },
+  blue300: {
+    backgroundColor: blue._300,
+  },
+  blue400: {
+    backgroundColor: blue._400,
+  },
+  blue500: {
+    backgroundColor: blue._500,
+  },
+  blue600: {
+    backgroundColor: blue._600,
+  },
+  blue700: {
+    backgroundColor: blue._700,
+  },
+  blue800: {
+    backgroundColor: blue._800,
+  },
+  blue900: {
+    backgroundColor: blue._900,
+  },
+  blue1000: {
+    backgroundColor: blue._1000,
+  },
+
+  /* RED */
+  red50: {
+    backgroundColor: red._50,
+  },
+  red100: {
+    backgroundColor: red._100,
+  },
+  red200: {
+    backgroundColor: red._200,
+  },
+  red300: {
+    backgroundColor: red._300,
+  },
+  red400: {
+    backgroundColor: red._400,
+  },
+  red500: {
+    backgroundColor: red._500,
+  },
+  red600: {
+    backgroundColor: red._600,
+  },
+  red700: {
+    backgroundColor: red._700,
+  },
+  red800: {
+    backgroundColor: red._800,
+  },
+  red900: {
+    backgroundColor: red._900,
+  },
+  red1000: {
+    backgroundColor: red._1000,
+  },
+
+  /* GREEN */
+  green50: {
+    backgroundColor: green._50,
+  },
+  green100: {
+    backgroundColor: green._100,
+  },
+  green200: {
+    backgroundColor: green._200,
+  },
+  green300: {
+    backgroundColor: green._300,
+  },
+  green400: {
+    backgroundColor: green._400,
+  },
+  green500: {
+    backgroundColor: green._500,
+  },
+  green600: {
+    backgroundColor: green._600,
+  },
+  green700: {
+    backgroundColor: green._700,
+  },
+  green800: {
+    backgroundColor: green._800,
+  },
+  green900: {
+    backgroundColor: green._900,
+  },
+  green1000: {
+    backgroundColor: green._1000,
+  },
+
+  /* YELLOW */
+  yellow50: {
+    backgroundColor: yellow._50,
+  },
+  yellow100: {
+    backgroundColor: yellow._100,
+  },
+  yellow200: {
+    backgroundColor: yellow._200,
+  },
+  yellow300: {
+    backgroundColor: yellow._300,
+  },
+  yellow400: {
+    backgroundColor: yellow._400,
+  },
+  yellow500: {
+    backgroundColor: yellow._500,
+  },
+  yellow600: {
+    backgroundColor: yellow._600,
+  },
+  yellow700: {
+    backgroundColor: yellow._700,
+  },
+  yellow800: {
+    backgroundColor: yellow._800,
+  },
+  yellow900: {
+    backgroundColor: yellow._900,
+  },
+  yellow1000: {
+    backgroundColor: yellow._1000,
+  },
+
+  /* GREY */
+  grey50: {
+    backgroundColor: grey._50,
+  },
+  grey100: {
+    backgroundColor: grey._100,
+  },
+  grey200: {
+    backgroundColor: grey._200,
+  },
+  grey300: {
+    backgroundColor: grey._300,
+  },
+  grey400: {
+    backgroundColor: grey._400,
+  },
+  grey500: {
+    backgroundColor: grey._500,
+  },
+  grey600: {
+    backgroundColor: grey._600,
+  },
+  grey700: {
+    backgroundColor: grey._700,
+  },
+  grey800: {
+    backgroundColor: grey._800,
+  },
+  grey900: {
+    backgroundColor: grey._900,
+  },
+  grey1000: {
+    backgroundColor: grey._1000,
+  },
+
+  /* WHITE */
+  white: {
+    backgroundColor: white._50,
+  },
+
+  /* BLACK */
+  black: {
+    backgroundColor: black._50,
+  },
+});
