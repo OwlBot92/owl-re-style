41563f73fc5283ae39586941b311c33913b68af0
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2022-06-17 02:54:19 +0200

    UPDATE:  imported files

--- src/index.js
+++ src/index.js
@@ -2,26 +2,41 @@
 import Button from './components/buttons/Button';
 
 /* STYLE */
+//size
 import { fSize } from './style/font/fSize';
+import { getScreenHeight, getScreenWidth } from './style/size/size';
+//alignment
 import { m } from './style/alignment/margin/margin';
 import { p } from './style/alignment/padding/padding';
-import { _, flexRow, flexColumn } from './style/alignment/flex/display'; // _ contains atomic flex options
-import fColor from './style/font/fColor';
-
+import { atomic, flexRow, flexColumn } from './style/alignment/flex/display'; // atomic contains atomic flex options
+//font
+import { fColor } from './style/font/fColor';
+import { fWeight } from './style/font/fWeight';
+//color
+import { bgColor } from './style/color/background/bgColor';
 
 export {
   // * STYLED COMPONENTS
   Button,
 
   // * STYLE
+  // size
+  getScreenHeight,
+  getScreenWidth,
+
   // font
   fSize,
   fColor,
+  fWeight,
 
   // alignment
   m,
   p,
-  _,
+  atomic,
   flexRow,
   flexColumn,
+
+  // color
+  bgColor,
 }
+
